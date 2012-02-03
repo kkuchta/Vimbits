ca9cfd214287ec2ed5a794beac62d5a6e62fb974
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-02-03 01:08:12 -0800

    Initial Revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vimbits::Application.config.secret_token = '3bc6f18ddd37951c660ed877213a2ee6c08db8438147dee6d85decd7439ca000ab8313d4bd3d2034877c7bf6e2791782db63ca066b23e67c4024615e54da990e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Vimbits::Application.config.session_store :cookie_store, key: '_vimbits_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Vimbits::Application.config.session_store :active_record_store

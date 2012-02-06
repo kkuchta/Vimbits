eb4503f7d721c9d431343bce9d08f2bc07db844f
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-02-06 00:07:50 -0800

    Added user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,23 @@ class UsersController < ApplicationController
     end  
   end  
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+
 end

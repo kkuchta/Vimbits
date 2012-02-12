2a3d0da25a7e4f06e387b063dcaba5ddd060e09d
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-02-12 14:07:33 -0800

    Made index presentable, and killed scaffold.css

--- app/helpers/bits_helper.rb
+++ app/helpers/bits_helper.rb
@@ -1,2 +1,10 @@
 module BitsHelper
+  def limit_string( str, lineLimit = 5, charLimit = 1000 )
+    lines = str[0,charLimit].split "\n"
+    logger.info "lines = " + lines.inspect
+    retVal = lines[0..4].join "\n"
+
+    retVal += "\n[...]" if retVal.length < str.length
+    return retVal
+  end
 end

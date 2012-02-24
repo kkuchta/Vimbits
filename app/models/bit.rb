0ff6e14c7002af4d6968f3f0581163e2054c4bc2
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-02-23 21:30:00 -0800

    precompiled

--- app/models/bit.rb
+++ app/models/bit.rb
@@ -1,5 +1,9 @@
 class Bit < ActiveRecord::Base
   validates_presence_of :code, :title, :user
+  validates :title, length: { minimum: 2, maximum: 160 }
+  validates :description, length: { maximum: 3000 }
+  validates :code, length: { minimum: 3, maximum: 2000 }
+  validates :password, length: { minimum: 3, maximum: 300 }
   attr_accessible :title, :code, :description, :user
   belongs_to :user
   after_create :vote_once

b52cae41b0709865c381d88accb05ff0cfd01c8f
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-02-26 13:39:08 -0800

    Unique usernames and emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 3, maximum: 300 }
   validates :username, length: { minimum: 3, maximum: 40 }
   validates :email, format: { with: /.+\@.+\..+/, message: 'Invalid email address.' }
+
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+
   attr_accessible :email, :password, :password_confirmation, :username
   has_many :bits
   acts_as_voter

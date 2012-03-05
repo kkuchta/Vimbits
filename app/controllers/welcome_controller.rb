1cd543680c094648dd040975d810889d3ad3c5ec
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-03-04 22:35:20 -0800

    Hot bits on front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @top = Bit.plusminus_tally.first()
     @new = Bit.order('created_at DESC').first()
+
+    hot_bits = Bit.where("bits.created_at > ?", 1.week.ago)
+    hot_bits = hot_bits.sort_by{ |bit| -1 * bit.hotness }
+    @hot = hot_bits.first
   end
 end

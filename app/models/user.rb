f708434371a14301d6f23f5f3b93ed8e733fd365
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-03-24 23:07:47 -0700

    Made +1 tab not show downvotes

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   def plusones
-    votes.on_bits.where("bits.user_id <> ?", id)
+    votes.on_bits.where("bits.user_id <> ? and vote=?", id, true)
   end
 
   def comments

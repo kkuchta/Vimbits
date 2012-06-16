c6ecc51270ebf3987133e0cf6e8649002fac01ff
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-06-16 15:20:52 -0700

    Increase hot threshold

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @new = Bit.order('created_at DESC').first()
 
-    hot_bits = Bit.where("bits.created_at > ?", 1.week.ago)
+    hot_bits = Bit.where("bits.created_at > ?", 3.week.ago)
     hot_bits = hot_bits.sort_by{ |bit| -1 * bit.hotness }
     @hot = hot_bits.first
   end
